14e8b311b0b8c6f1f691e1fff105d06c651691d8
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-03 03:16:14 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ char alph;
 
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-for (alph = 'a'; alph <= 'p'; alph++)
+for (alph = 'a'; alph <= 'f'; alph++)
 putchar(alph);
 putchar('\n');
 return (0);
