d6767a6f1fc6c3f6b9bbadab7c15b30e6c997149
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-05 19:44:33 -0700

    fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,21 @@
 * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
 */
 void print_alphabet_x10(void)
+
 {
 int count = 0;
 char letter = 'a';
 
 while (count < 10)
+
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
+ 
 }
+
 count++;
+
 }
+
