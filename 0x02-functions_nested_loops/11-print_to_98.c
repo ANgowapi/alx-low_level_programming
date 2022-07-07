953c21a18f7fd5b9cbf4eeed0e4f0ae3415cc509
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-06 20:03:09 -0700

    98 Battery Street the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - prints all natural numbers from n to 98
+* followed by a new line.
+* @n: n is the starting integer.
+*
+* Return: void
+*/
+
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (n = n; n <= 97; n++)
+printf("%d, ", n);
+}
+else
+{
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("98\n");
+}
+}
