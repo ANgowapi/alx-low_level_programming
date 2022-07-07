0e13138d239b247eeb1714423e98dd5bd7b73d65
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-06 21:31:24 -0700

    Hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,23 +25,23 @@ if (product < 10)
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(product % 10 + '0');
+_putchar(product % 10);
 }
 else if (product >= 10 && product < 100)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar((product / 10) % 10 + '0');
-_putchar(product % 10 + '0');
+_putchar((product / 10) % 10);
+_putchar(product % 10);
 }
 else if (product > 99 && product < 1000)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(product / 100 + '0');
-_putchar((product / 10) % 10 + '0');
-_putchar(product % 10 + '0');
+_putchar(product / 100);
+_putchar((product / 10) % 10);
+_putchar(product % 10);
 }
 }
 _putchar('\n');
