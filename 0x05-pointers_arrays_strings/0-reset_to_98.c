4e7e53dc31e2c5e9c2f8657a7eee4e9da3780502
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-12 16:57:56 -0700

    Reset function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 * reset_to_98.c - takes a pointer to an int as parameter
 * and updates the value it points to to 98.
-*
+* @n: n is the pointer to the integer.
 * Return: void
 */
 void reset_to_98(int *n)
