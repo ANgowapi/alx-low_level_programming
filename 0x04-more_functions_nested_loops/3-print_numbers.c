7eae4b01c46a7039e9c894515723b244722eccc9
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-13 07:55:43 -0700

    print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
 * print_numbers - Prints the numbers from 0 up to 9
+* @a: a is an integer
 *
 * Return: void
 */
@@ -12,6 +13,5 @@ for (a = 0; a <= 9; a++)
 {
 _putchar(a + '0');
 }
-
 _putchar('\n');
 }
