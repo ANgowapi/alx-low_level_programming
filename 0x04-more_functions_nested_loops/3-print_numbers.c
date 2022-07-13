adbb46b157d3d7f08d6c1766f65b064276a6112a
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-13 08:04:26 -0700

    print_numbers fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
 * print_numbers - Prints the numbers from 0 up to 9
-* @a: a is an integer
 *
 * Return: void
 */
 void print_numbers(void)
 {
-int a;
-for (a = 0; a <= 9; a++)
+char a;
+for (a = '0'; a <= '9'; a++)
 {
-_putchar(a + '0\n');
+_putchar(a);
 }
-
+_putchar('\n');
 }
