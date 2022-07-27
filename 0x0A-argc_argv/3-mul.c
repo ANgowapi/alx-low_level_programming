90147de1b8eed213d9a709e45486431380d5444b
ANgowapi <aphiwe.ngowapi@outlook.com>
2022-07-27 15:04:37 -0700

    fourth commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: argument counter
+ * @argv: array of pointer to strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int a, b, c;
+
+a = 1;
+b = 2;
+c = 3;
+
+if (argc != c)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[a]) * atoi(argv[b]));
+return (0);
+}
